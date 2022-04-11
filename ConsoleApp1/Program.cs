6ff1a4cc6f5246574fd0ad16e297534de65ccd82
Abdul Mateen <abdulmateenkhanzwl@gmail.com>
2022-04-11 22:46:37 +0500

    chk

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //chk
             List<Ship> ship_list = new List<Ship>();
             char op;
             while(true)
